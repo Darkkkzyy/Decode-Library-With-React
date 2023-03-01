ca4a515358ad7aba998ad65c28e6ca070ded24bc
Jonathan <jonhenrilu@gmail.com>
2023-03-01 16:58:20 +0000

    FIxed bug reponsivity

--- src/pages/livros/index.tsx
+++ src/pages/livros/index.tsx
@@ -13,10 +13,10 @@ const Livros = () => {
     <>
       <Navbar />
       {/* container principal */}
-      <div className="flex h-screen w-full flex-col items-center justify-center">
+      <div className="flex h-screen w-full flex-col items-center ">
        {/* top */}
         <div className="flex flex-col items-center justify-center w-[80%]">
-          <h1 className="text-6xl self-start mb-[100px] text-white">A nossa biblioteca</h1>
+          <h1 className="m-10 text-6xl self-start  whitespace-nowrap text-white sm:mt-[50px] text-3xl">A nossa biblioteca</h1>
           {/* searchContainer */}
           <div className="border-none">
             <Input
@@ -35,7 +35,7 @@ const Livros = () => {
         </div>
         {/* Books */}
         <div className="flex flex-row flex-wrap justify-center items-center">
-          <div className="mr-[24px] flex h-[480px] flex-col">
+          <div className="hidden md:mr-[24px] md:flex h-[480px] flex-col ">
             <button className=" mb-3 flex  h-[381px] w-[261px] items-center justify-center border">
               <Image src={plusimg} className="w-[38px]" alt="plus icon"></Image>
             </button>
@@ -47,7 +47,7 @@ const Livros = () => {
               <Image
                 onClick={() => setClickedBook("war-peace")}
                 src={capaLivro}
-                className="h-[381px] w-[261px]"
+                className="h-[160px] w-[110px] md:h-[381px] md:w-[261px]"
                 alt="capaLivro"
               ></Image>
             </Link>
