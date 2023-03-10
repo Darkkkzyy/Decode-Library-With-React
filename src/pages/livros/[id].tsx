7feef9a0d0f266bb6e6b7c91f49beaa3a7193c6b
Jonathan <jonhenrilu@gmail.com>
2023-03-10 14:02:51 +0000

    mocks books

--- src/pages/livros/[id].tsx
+++ src/pages/livros/[id].tsx
@@ -1,5 +1,6 @@
 import Card from "~/components/card";
 import Navbar from "~/components/navbar";
+import BooksMocks from "../../mocks/books"
 
 const Details = () => {
   return (
@@ -9,7 +10,9 @@ const Details = () => {
         <h1 className="m-10 self-center whitespace-nowrap  text-3xl text-white sm:mt-[50px] sm:self-start sm:text-6xl">
           Details Page
         </h1>
-        <Card color autor="teste" price={24}title="ola"/>
+        {
+         BooksMocks.filter((book => book.id === id))
+        }
       </div>
     </>
   );
