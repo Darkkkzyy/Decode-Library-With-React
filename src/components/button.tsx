0e3cd6b0aad0de49a95879e93854dd48d4d5a72e
Jonathan <jonhenrilu@gmail.com>
2023-03-14 10:28:15 +0000

    Add evento to Button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,14 +6,15 @@ interface Props {
   tranparent?: boolean;
   visibility?:boolean;
   bgColor?:boolean;
+  onClick: () => void;
 }
 
-const Button = ({ children, tranparent,width,visibility,bgColor}: Props) => {
+const Button = ({ children, tranparent,width,visibility,bgColor,onClick}: Props) => {
    return (//justify-center
 
 <>
     <div className="flex items-center ">  
-      <button 
+      <button onClick={onClick}
         className={`h-[43px] w-[360px] rounded-full ${tranparent  ? "border border-white bg-transparent" : "bg-[#FF4E16]  hover:bg-orange-700" } py-2 px-4 font-bold text-white
         ${
           width
