2e893d1bca0c70c9a97aa627d1bd994d0bc54f7b
Jonathan <jonhenrilu@gmail.com>
2023-03-14 10:37:50 +0000

    input show icon

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,16 +1,23 @@
+import { FaSearch } from "react-icons/fa";
+
 interface Props {
   label: string;
+  showIcon?:boolean;
 }
 
-const Input = ({label}:Props) => {
-    return ( <>
-    <div>
-    <input
-      className="rounded-[100px] border-2 w-[360px] h-[46px] p-5 m-[10px] "
-      placeholder={label} />
-    </div>
-    
-    </> );
-}
- 
+const Input = ({ label, showIcon }: Props) => {
+  return (
+    <>
+      <div>
+        <input
+          className="m-[10px] h-[46px] w-[360px] rounded-[100px] border-2 p-5 "
+          placeholder={label}
+        />
+        {/* Se showIcon for true, o ícone será exibido. */}
+      {showIcon && <FaSearch className="icon "  />}
+      </div>
+    </>
+  );
+};
+
 export default Input;
