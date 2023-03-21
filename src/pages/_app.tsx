22881558d2aba1d765eff3a8f8aca99eb77a87ce
Jonathan <jonhenrilu@gmail.com>
2023-03-21 11:54:56 +0000

    i18n

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
+import { I18nextProvider } from "react-i18next";
+import i18n from "../i18n/i18n";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <I18nextProvider i18n={i18n}>
+      <Component {...pageProps} />
+    </I18nextProvider>
+  );
 };
 
 export default MyApp;
